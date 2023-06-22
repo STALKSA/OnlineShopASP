4685925dd35266bb4efe50507e5f7e3d9615a2f8
Ksenia <109988277+STALKSA@users.noreply.github.com>
2023-06-22 12:45:39 +0300

    Update SmtpConfig.cs

--- OnlineShopASP/SmtpConfig.cs
+++ OnlineShopASP/SmtpConfig.cs
@@ -4,15 +4,15 @@ namespace OnlineShopASP
 {
     public class SmtpConfig
     {
-        [Required, RegularExpression(@"^[a-zA-Z0-9\.\-_]+@example\.com$")] //хост в формате from_address@example.com
+        [Required, RegularExpression(@"^[a-zA-Z0-9\.\-_]+@example\.com$", ErrorMessage = "Невалидный Host формат")] //хост в формате from_address@example.com
         public string Host { get; set; }
-        [Required]
+        [Required, RegularExpression(@"^[a-zA-Z0-9\.\-_]+$", ErrorMessage = "Невалидный UserName формат")]
         public string UserName { get; set; }
-        [Required]
+        [Required, RegularExpression(@"^[a-zA-Z0-9\.\-_]+$", ErrorMessage = "Невалидный Password формат")]
         public string Password { get; set; }
-        [Required, Range(1, ushort.MaxValue)]
+        [Required, Range(1, ushort.MaxValue, ErrorMessage = "Невалидное Port значение")]
         public int Port { get; set; }
-        [Required, EmailAddress]
+        [Required, EmailAddress(ErrorMessage = "Невалидный FromEmail формат")]
         public string FromEmail { get; set; }
     }
 }
