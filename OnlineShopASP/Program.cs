800d25b5664ae43d117845730a4cfb04ac5b9eac
ksenia <ksy184694@gmail.com>
2023-06-23 19:23:09 +0300

    adding endpoint

--- OnlineShopASP/Program.cs
+++ OnlineShopASP/Program.cs
@@ -18,13 +18,10 @@ builder.Services.AddOptions<SmtpConfig>()
     .ValidateOnStart();
 
 
-builder.Services.AddSingleton<ICatalog, InMemoryCatalog>();                       // Регистрация зависимости каталог
-//builder.Services.AddSingleton<IClock, UtcClock>();                               // Регистрация зависимости Time
-
-
-builder.Services.AddScoped<IEmailSender, MailKitSmtpEmailSender>();              //Регистрация отправки почты
-builder.Services.Decorate<IEmailSender, EmailSenderLoggingDecorator>();         //Декоратор
-
+builder.Services.AddSingleton<ICatalog, InMemoryCatalog>();                          // Регистрация зависимости каталог
+//builder.Services.AddSingleton<IClock, UtcClock>();                                // Регистрация зависимости Time
+builder.Services.AddScoped<IEmailSender, MailKitSmtpEmailSender>();                //Регистрация отправки почты
+builder.Services.Decorate<IEmailSender, EmailSenderLoggingDecorator>();           //Декоратор
 //builder.Services.AddHostedService<AppStartedNotificatorBackgroundService>();   //Регистрация фонового сервиса
 builder.Services.AddHostedService<SalesNotificatorBackgroundService>();     
 
@@ -47,13 +44,12 @@ app.MapPost("/update_product", UpdateProductById);
 app.MapPost("/delete_product", DeleteProductById);
 app.MapPost("/clear_products", ClearProducts);
 
-//app.MapGet("send_email", SendEmail);
+app.MapGet("/send_email", SendEmail);
 
-//async Task SendEmail(IEmailSender emailSender, HttpContext context)
-//{
-//    await emailSender.SendEmailAsync("to_address@example.com", "Приложение запущено", "Приложение запущено");
-//    context.Response.StatusCode = StatusCodes.Status200OK;
-//}
+async Task SendEmail(string recepientEmail, string subject, string message, IEmailSender emailSender)
+{
+    await emailSender.SendEmail(recepientEmail, subject, message);
+}
 
 void AddProduct(Product product,ICatalog catalog, HttpContext context)
 {
